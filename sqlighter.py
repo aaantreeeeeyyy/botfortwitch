550f01f657e6c1dc9b293ee57f3b07178ed2803f
Andrey Rakow <anraow@gmail.com>
2021-05-07 17:16:38 +0300

    last update

--- sqlighter.py
+++ sqlighter.py
@@ -1,10 +1,16 @@
-import sqlite3
+import os
+import psycopg2
 
 
 class SQLighter:
-    def __init__(self, database):
-        """Подключаемся к БД и сохраняем курсор соединения"""
-        self.connection = sqlite3.connect(database)
+    def __init__(self):
+        DATABASE_URL = os.environ['jdbc:postgresql://localhost:5432/postgres']
+        self.connection = psycopg2.connect(DATABASE_URL, sslmode='require')
+        # self.connection = psycopg2.connect(
+        #     host="localhost",
+        #     database="postgres 2",
+        #     user="postgres",
+        #     password="9xvG2cn")
         self.cursor = self.connection.cursor()
 
     def get_subscriptions(self, status=True):
@@ -27,11 +33,12 @@ class SQLighter:
         """Обновляем статус подписки пользователя"""
         with self.connection:
             return self.cursor.execute("UPDATE `profile` SET `status` = ? WHERE `user_id` = ?", (status, user_id))
-    
+
     def update_name(self, user_id, name):
         with self.connection:
-            return self.cursor.execute("UPDATE `profile` SET `username` = ? WHERE `user_id` = ?", (name, user_id))
-    
+            return self.cursor.execute("INSERT INTO `profile` (`user_id`, `username`) VALUES(?,?)", (user_id, name))
+
     def close(self):
         """Закрываем соединение с БД"""
         self.connection.close()
+        self.cursor.close()
